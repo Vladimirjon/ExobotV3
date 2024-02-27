2f654092156daa521c84f13663ff4477b3e13379
Vladimirjon <johannpasquel@gmail.com>
2024-02-27 08:53:53 -0500

    BL

--- src/BussinessLogic/ExobotBL.java
+++ src/BussinessLogic/ExobotBL.java
@@ -1,5 +1,29 @@
 package BussinessLogic;
+import java.util.List;
+import DataAccess.DTO.ExobotDTO;
+import DataAccess.ExobotDAO;
 
 public class ExobotBL {
+    private ExobotDTO exobotDTO;
+    private ExobotDAO exobotDAO = new ExobotDAO();
+
+    public ExobotBL(){}
+    
+    public List<ExobotDTO> getAll() throws Exception{
+        return exobotDAO.readAll();
+    }
+    public ExobotDTO getByIdDato(int idExobot) throws Exception{
+        exobotDTO = exobotDAO.readBy(idExobot);
+        return exobotDTO;
+    }
+    public boolean create(ExobotDTO eDTO) throws Exception{   
+        return exobotDAO.create(eDTO);
+    }
+    public boolean update(ExobotDTO eDTO) throws Exception{
+        return exobotDAO.update(eDTO);
+    }
+    public boolean delete(int idExobot) throws Exception{
+        return exobotDAO.delete(idExobot);
+    }
     
 }
--- src/BussinessLogic/SoldadoBL.java
+++ src/BussinessLogic/SoldadoBL.java
@@ -1,5 +1,30 @@
 package BussinessLogic;
+import java.util.List;
+import DataAccess.DTO.SoldadoDTO;
+import DataAccess.SoldadoDAO;
 
 public class SoldadoBL {
+    private SoldadoDTO soldado;
+    private SoldadoDAO soldadoDAO = new SoldadoDAO();
+
+    public SoldadoBL(){}
+    
+    public List<SoldadoDTO> getAll() throws Exception{
+        return soldadoDAO.readAll();
+    }
+    public SoldadoDTO getByIdDato(int idExobot) throws Exception{
+        soldado = soldadoDAO.readBy(idExobot);
+        return soldado;
+    }
+    public boolean create(SoldadoDTO sDTO) throws Exception{   
+        return soldadoDAO.create(sDTO);
+    }
+    public boolean update(SoldadoDTO sDTO) throws Exception{
+        return soldadoDAO.update(sDTO);
+    }
+    public boolean delete(int idExobot) throws Exception{
+        return soldadoDAO.delete(idExobot);
+    }
     
 }
+
